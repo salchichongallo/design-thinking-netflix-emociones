0ec9bfe25c3366947a98562b422f2759ed235508
Jimmy Murillo <salchichongallo1997@gmail.com>
2025-07-02 23:51:37 -0500

    feat(video): play selected video

--- src/pages/home/home.page.ts
+++ src/pages/home/home.page.ts
@@ -1,12 +1,13 @@
 import { Router } from "@angular/router";
 import { Component, inject } from "@angular/core";
 import { EmotionOptionComponent } from "../../components/emotion-option/emotion-option.component";
+import { AppHeaderComponent } from "../../components/app-header/app-header.component";
 
 @Component({
   selector: "home-page",
   templateUrl: "./home.page.html",
   styleUrl: "./home.page.scss",
-  imports: [EmotionOptionComponent],
+  imports: [AppHeaderComponent, EmotionOptionComponent],
 })
 export class HomePage {
   private router = inject(Router);
--- src/pages/video/video.page.ts
+++ src/pages/video/video.page.ts
@@ -1,9 +1,23 @@
-import { Component, input } from "@angular/core";
+import { AfterViewInit, Component, computed, input } from "@angular/core";
+import { AppHeaderComponent } from "../../components/app-header/app-header.component";
 
 @Component({
   selector: "video-page",
   templateUrl: "./video.page.html",
+  styleUrl: "./video.page.scss",
+  imports: [AppHeaderComponent],
 })
-export class VideoPage {
+export class VideoPage implements AfterViewInit {
   videoName = input.required();
+
+  videoUrl = computed(() => `/videos/${this.videoName()}`);
+
+  ngAfterViewInit() {
+    document.querySelector("video")?.requestFullscreen();
+  }
+
+  onFinish() {
+    document.exitFullscreen();
+    alert("Finished!");
+  }
 }
