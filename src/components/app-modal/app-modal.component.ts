6d0e0d8e1aa268b4fd66c19b88d90ca0c0e3a25e
Jimmy Murillo <salchichongallo1997@gmail.com>
2025-07-03 07:56:35 -0500

    refactor(video): extract modal component

---
+++ src/components/app-modal/app-modal.component.ts
@@ -0,0 +1,8 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: "app-modal",
+  templateUrl: "./app-modal.component.html",
+  styleUrl: "./app-modal.component.scss",
+})
+export class AppModalComponent {}
--- src/components/video-survey/video-survey.component.ts
+++ src/components/video-survey/video-survey.component.ts
@@ -4,12 +4,13 @@ import { Component, inject, input, signal } from "@angular/core";
 
 import { AppVideo } from "../../videos";
 import { EmotionOptionComponent } from "../emotion-option/emotion-option.component";
+import { AppModalComponent } from "../app-modal/app-modal.component";
 
 @Component({
   selector: "video-survey",
   templateUrl: "./video-survey.component.html",
   styleUrl: "./video-survey.component.scss",
-  imports: [EmotionOptionComponent, NgIf, RouterLink],
+  imports: [EmotionOptionComponent, NgIf, RouterLink, AppModalComponent],
 })
 export class VideoSurveyComponent {
   private router = inject(Router);
