e15ae9bdad665142011dc85db4f3f559e9108a31
Jimmy Murillo <salchichongallo1997@gmail.com>
2025-07-03 08:22:35 -0500

    feat(app-modal): add transparent input

--- src/components/app-modal/app-modal.component.ts
+++ src/components/app-modal/app-modal.component.ts
@@ -1,8 +1,10 @@
-import { Component } from "@angular/core";
+import { Component, input } from "@angular/core";
 
 @Component({
   selector: "app-modal",
   templateUrl: "./app-modal.component.html",
   styleUrl: "./app-modal.component.scss",
 })
-export class AppModalComponent {}
+export class AppModalComponent {
+  transparent = input(false);
+}
