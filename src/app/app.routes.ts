bc95f0ffa0bf5f54de5d4ba668093947edc8aca6
Jimmy Murillo <salchichongallo1997@gmail.com>
2025-07-13 18:04:54 -0500

    feat(landing): init page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,16 +1,30 @@
 import { Routes } from "@angular/router";
 
-export const routes: Routes = [
-  {
+const routes: Routes = [];
+
+const ONLY_LANDING = true;
+if (ONLY_LANDING) {
+  routes.push({
     path: "",
     pathMatch: "full",
     loadComponent: () =>
-      import("../pages/home/home.page").then((m) => m.HomePage),
-  },
-  {
-    path: "video/:videoName",
-    pathMatch: "full",
-    loadComponent: () =>
-      import("../pages/video/video.page").then((m) => m.VideoPage),
-  },
-];
+      import("../pages/landing/landing.page").then((m) => m.LandingPage),
+  });
+} else {
+  routes.push(
+    {
+      path: "",
+      pathMatch: "full",
+      loadComponent: () =>
+        import("../pages/home/home.page").then((m) => m.HomePage),
+    },
+    {
+      path: "video/:videoName",
+      pathMatch: "full",
+      loadComponent: () =>
+        import("../pages/video/video.page").then((m) => m.VideoPage),
+    }
+  );
+}
+
+export { routes };
---
+++ src/pages/landing/landing.page.ts
@@ -0,0 +1,8 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: "landing-page",
+  templateUrl: "./landing.page.html",
+  styleUrl: "./landing.page.scss",
+})
+export class LandingPage {}
