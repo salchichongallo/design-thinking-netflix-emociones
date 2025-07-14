079b8e92124fa24bfc99007964c376770b6ef64b
Jimmy Murillo <salchichongallo1997@gmail.com>
2025-07-13 21:26:35 -0500

    feat(landing): layout page

--- src/pages/landing/landing.page.ts
+++ src/pages/landing/landing.page.ts
@@ -1,8 +1,11 @@
 import { Component } from "@angular/core";
+import { AppHeaderComponent } from "../../components/app-header/app-header.component";
+import { EmotionOptionComponent } from "../../components/emotion-option/emotion-option.component";
 
 @Component({
   selector: "landing-page",
   templateUrl: "./landing.page.html",
   styleUrl: "./landing.page.scss",
+  imports: [AppHeaderComponent, EmotionOptionComponent],
 })
 export class LandingPage {}
