9c15a49883a64523630e3d83875bda91fee05c44
Jimmy Murillo <salchichongallo1997@gmail.com>
2025-07-20 10:53:47 -0500

    feat(for-you): add swipe events

--- src/prototype/pages/for-you/for-you.page.ts
+++ src/prototype/pages/for-you/for-you.page.ts
@@ -1,5 +1,6 @@
 import { RouterLink } from "@angular/router";
-import { Component, input } from "@angular/core";
+import { Subscription, fromEvent, map } from "rxjs";
+import { Component, ElementRef, input, ViewChild } from "@angular/core";
 import { ScrollUsage } from "../../components/scroll-usage/scroll-usage.component";
 
 @Component({
@@ -10,4 +11,58 @@ import { ScrollUsage } from "../../components/scroll-usage/scroll-usage.componen
 })
 export class ForYouPage {
   emotionId = input.required<string>();
+
+  @ViewChild("videoPlayer") videoRef!: ElementRef<HTMLVideoElement>;
+
+  private subscription = new Subscription();
+
+  ngAfterViewInit() {
+    this.listenSwipeEvents();
+  }
+
+  private listenSwipeEvents() {
+    let startY = 0;
+    const swipeTreshold = 10;
+    const touchStart$ = fromEvent<TouchEvent>(this.video, "touchstart").pipe(
+      map((event) => event.touches[0].clientY)
+    );
+    const touchEnd$ = fromEvent<TouchEvent>(this.video, "touchend").pipe(
+      map((event) => event.changedTouches[0].clientY)
+    );
+    this.subscription.add(touchStart$.subscribe((y) => (startY = y)));
+    this.subscription.add(
+      touchEnd$.subscribe((endY) => {
+        const deltaY = startY - endY;
+        if (deltaY > swipeTreshold) {
+          this.onSwipeUp();
+        } else if (deltaY < -swipeTreshold) {
+          this.onSwipeDown();
+        }
+      })
+    );
+  }
+
+  private onSwipeUp() {
+    this.previousVideo();
+  }
+
+  private onSwipeDown() {
+    this.nextVideo();
+  }
+
+  previousVideo() {
+    // ...
+  }
+
+  nextVideo() {
+    // ...
+  }
+
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
+  }
+
+  get video() {
+    return this.videoRef.nativeElement;
+  }
 }
